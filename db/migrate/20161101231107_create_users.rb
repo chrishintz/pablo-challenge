28a734fe2b26f4b10debdeaefc16385b9732ed56
Chris Hintz <chrishintz42@gmail.com>
2016-11-01 16:27:37 -0700

    Add models for Users & Books

---
+++ db/migrate/20161101231107_create_users.rb
@@ -0,0 +1,16 @@
+class CreateUsers < ActiveRecord::Migration[5.0]
+  def up
+    create_table :users do |t|
+      t.string "username"
+      t.string "first_name"
+      t.string "last_name"
+      t.string "password_digest"
+      t.string "email"
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :users
+  end
+end
