c828e075c9c8466556f189760f7ddb5a27ac5659
Chris Hintz <chrishintz42@gmail.com>
2016-11-01 17:58:28 -0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'user#index'
+
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  post 'users/create'
+  get 'users/edit'
+  patch 'users/update'
+  get 'users/delete'
+  delete 'users/delete'
+
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  post 'books/create'
+  get 'books/edit'
+  patch 'books/update'
+  get 'books/delete'
+  delete 'books/delete'
+
 end
