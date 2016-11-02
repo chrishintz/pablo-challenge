9c98f0201eb4b380716bac510f4f5c7b279a9ac1
Chris Hintz <chrishintz42@gmail.com>
2016-11-02 07:17:28 -0700

    Add validations and other

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,7 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  validates :title,  :presence => true
+  validates :author, :length => { maximum: 50 }
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :books
+
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+
+  validates :first_name, :last_name,  :presence => true,
+                                      :length => { :maximum => 25 }
+  validates :username, :length => { :within => 5..25 },
+                       :uniqueness => true
+  validates :email,     :presence => true,
+                        :length => { :maximum => 100 },
+                        :format => EMAIL_REGEX
 end
